4081f412f23fb3abca03a560e1e0dc5c21f3c82b
jacksangl <sangljack06@gmail.com>
2025-09-16 19:51:59 -0400

    Deploying

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,5 @@ app.delete('/todos/:id', async (req, res) => {
   res.json({ deleted: true });
 });
 
-app.listen(5001, () => console.log('Server running on port 5001'));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
